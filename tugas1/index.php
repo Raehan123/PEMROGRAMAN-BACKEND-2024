e78fc3cd651e5ed318709353a466be9a7ce7f422
Raehan123 <nurrayhan62@gmail.com>
2024-10-14 13:52:56 +0700

    Tugas Pertemuan 3

--- tugas1/index.php
+++ tugas1/index.php
@@ -1,41 +1,44 @@
 <?php
 
-class Animal {
+class Animal
+{
     // Property animals (array)
     public $animals;
 
     // Constructor untuk mengisi data awal animals
-    public function __construct() {
-        $this->animals = ['Ayam','Ikan'];
+    public function __construct()
+    {
+        $this->animals = ['Ayam', 'Ikan'];
     }
 
     // Method untuk menampilkan seluruh data animals
-    public function index() {
+    public function index()
+    {
         foreach ($this->animals as $index => $animals) {
         }
     }
 
     // Method untuk menambahkan hewan baru
-    public function store($animal) {
+    public function store($animal)
+    {
         if (is_string($animal)) {
             array_push($this->animals, $animal);
         }
     }
 
     // Method untuk memperbarui data hewan
-    public function update($index, $newAnimal) {
+    public function update($index, $newAnimal)
+    {
         if (isset($this->animals[$index])) {
-            $oldAnimal = $this->animals[$index];
             $this->animals[$index] = $newAnimal;
         }
     }
 
     // Method untuk menghapus data hewan
-    public function destroy($index) {
+    public function destroy($index)
+    {
         if (isset($this->animals[$index])) {
-            $deletedAnimal = $this->animals[$index];
             array_splice($this->animals, $index, 1);
-            $this->animals = array_values($this->animals);
         }
     }
 }
@@ -43,33 +46,38 @@ class Animal {
 $animal = new Animal();
 
 // Menampilkan seluruh data animals
-echo 'Index - Menampilkan seluruh data hewan ';
+echo 'Index - Menampilkan seluruh data hewan <br/>';
 $animal->index();
 
 print_r($animal->animals);
 echo PHP_EOL;
+echo '<br/>';
 
 // Menambahkan hewan baru
-echo 'Store - Menambahkan data hewan baru ';
+echo 'Store - Menambahkan data hewan baru <br/>';
 $animal->store('Burung');
 $animal->index();
 
 print_r($animal->animals);
 echo PHP_EOL;
+echo '<br/>';
+
 
 // Memperbarui data hewan
-echo 'Update - Mengupdate data hewan ';
+echo 'Update - Mengupdate data hewan <br/>';
 $animal->update(0, 'Kucing Anggora');
 $animal->index();
 
 print_r($animal->animals);
 echo PHP_EOL;
+echo '<br/>';
 
 
 // Menghapus data hewan
-echo 'Destroy - Menghapus data hewan ';
+echo 'Destroy - Menghapus data hewan <br/>';
 $animal->destroy(1);
 $animal->index();
+
 
 print_r($animal->animals);
 echo PHP_EOL;
